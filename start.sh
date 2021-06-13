388faf517d5dbb26cdd876f11c5a4e2ed3cfa060
Pushpender Rana <pushpender661@gmail.com>
2021-06-14 01:46:51 +0530

    Update yaml

--- start.sh
+++ start.sh
@@ -1,2 +1,4 @@
 #!/bin/bash
-ttyd/ttyd -p 8580 -i lo bash & sudo websockify/run 6080 --token-plugin JWTTokenApi & python3 hypervisor/manage.py runserver & python3 docker-console/TerminalSocket.py
+#ttyd/ttyd -p 8580 -i lo bash & sudo websockify/run 6080 --token-plugin JWTTokenApi & python3 hypervisor/manage.py runserver & python3 docker-console/TerminalSocket.py
+
+docker-compose up & ttyd/ttyd -p 8580 -i lo bash
